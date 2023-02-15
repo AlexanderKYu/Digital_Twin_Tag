fe95369cbb9ebf39da1d41bba798e00ac012204c
aloisclerc <alois.clerc@gmail.com>
2023-02-15 10:46:21 -0500

    basic front and back end connection

---
+++ react-front-end/src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [socketInstance, setSocketInstance] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [connected, setConnected] = useState(false);
+  const [data, setData] = useState("");
+
+  const handleClick = () => {
+    if (connected === false) {
+      setConnected(true);
+    } else {
+      setConnected(false);
+    }
+  };
+
+  useEffect(() => {
+    if (connected === true) {
+      fetch('/http-call').then(res => res.json()).then(data => {
+        setData(data.data);
+      });
+      const socket = io("localhost:5000/", {
+        transports: ["websocket"],
+        cors: {
+          origin: "http://localhost:3000/",
+        },
+      });
+
+      setSocketInstance(socket);
+
+      socket.on("connect", (data) => {
+        console.log(data);
+      });
+
+      setLoading(false);
+
+      socket.on("disconnect", (data) => {
+        console.log(data);
+      });
+
+      return function cleanup() {
+        socket.disconnect();
+      };
+    }
+  }, [connected]);
+
+  return (
+    <div className="App">
+      <h1>Digital Twin Tag</h1>
+      <button onClick={handleClick}>Click To Call API</button>
+      <p>{data}</p>
+    </div>
+  );
+}
+
+export default App;
