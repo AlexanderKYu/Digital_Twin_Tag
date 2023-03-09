52a9a31cf232ba691673fa530c2e8d685b275a56
AlexanderKYu <alexanderkyu27@gmail.com>
2023-03-09 10:42:38 -0500

    removed deleted files

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import PyQt5.QtWidgets as QtWidgets
-from PyQt5.uic import loadUi
-import QTWidgets.QDialog as QDialog
-import QTWidgets.QApplication as QApplication
-import QTWidgets.QWidget as QWidget
-
-
-
--- player_acceleration.py
+++
@@ -1,1 +0,0 @@
-#with the speed calculated in the previous script we can use recursion to calculate the acceleration by comparing it to the old acceleration 
