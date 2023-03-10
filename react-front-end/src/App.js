4d3e8af3067c6d1225caf9e2ab78dc01f4bb560d
Alois Clerc <alois.clerc@gmail.com>
2023-03-10 12:20:40 -0500

    main fix

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -20,26 +20,6 @@ function App() {
   const [connected, setConnected] = useState(false);
   const [data, setData] = useState("");
 
-  const testString = "WIP TEST";
-
-  const [data, setData] = useState("");
-
-  const handleClick = () => {
-    var jsonData = { 
-      "tagNumber": "0x001038",
-      "wipNumber": "111111",
-  }
-
-  const aliasData = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(jsonData)
-  };
-
-  fetch('/link-wip', aliasData).then(res => res.json()).then(data => {
-      setData(data.data);
-    });
-  }
 
   useEffect(() => {
       // create websocket/connect
@@ -59,14 +39,7 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <div className="App">
-        {/* <Nav />
-        <h1>Welcome to this React Application</h1> */}
-        {/* <h1>Digital Twin Tag</h1>
-        <button onClick={handleClick}>Click To Call API</button>
-        <p>{data}</p> */}
         <Nav></Nav>
-        <button onClick={handleClick}> Send Tag</button>
-        {data}
         <WIP></WIP>
     </div>
     </ChakraProvider>
