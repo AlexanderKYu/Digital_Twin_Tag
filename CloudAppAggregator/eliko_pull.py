207c3e89ddc4e2efe0c848ca17d70b7f950baa15
AlexanderKYu <alexanderkyu27@gmail.com>
2023-03-31 13:08:06 -0400

    update cloud branch, will make seperate db branch

--- CloudAppAggregator/eliko_pull.py
+++ CloudAppAggregator/eliko_pull.py
@@ -4,8 +4,6 @@ import os.path
 import datetime
 import json
 import pickle
-import Eliko.JSON_eliko_call as ecall
-import database.dbfuncs as dbcall
 
 REGEX_PAT = "[0-9a-fA-F]x[0-9a-fA-F]{6}.+\n"
 REGEX_ID_PAT = "[0-9a-fA-F]x[0-9a-fA-F]{6}"
@@ -30,7 +28,7 @@ def dbTagsPush(tagsJson):
     return tagsJson
 
 def compare_data_values(past, curr):
-    curr = json.loads(curr)
+    # curr = json.loads(curr)
     index = 0
 
     overall_diff = {}
@@ -79,70 +77,62 @@ def compare_data_values(past, curr):
     return False
         
 
-# def pullData():
-#     lines = []
-#     with open("sampleSets/Get_Tags.log", "r") as file:
-#         lines = file.readlines()
-#     
-#     tag_data = {}
-#
-#     for line in lines:
-#         data_lines = re.findall(REGEX_PAT, line)
-#         for data in data_lines:
-#             data = data[:-1]
-#             data = data.split(",")
-#             tag = data[0]
-#             x_coord = float(data[6])
-#             y_coord = float(data[7])
-#             z_coord = float(data[8])
-#             unix_time = float(data[5])
-#             tag_data[tag] = [x_coord, y_coord, z_coord, unix_time]
-#
-#     return tag_data
+def pullData():
+    lines = []
+    with open("sampleSets/Get_Tags.log", "r") as file:
+        lines = file.readlines()
+    
+    tag_data = {}
+
+    for line in lines:
+        data_lines = re.findall(REGEX_PAT, line)
+        for data in data_lines:
+            data = data[:-1]
+            data = data.split(",")
+            tag = data[0]
+            x_coord = float(data[6])
+            y_coord = float(data[7])
+            z_coord = float(data[8])
+            unix_time = float(data[5])
+            tag_data[tag] = [x_coord, y_coord, z_coord, unix_time]
+
+    return tag_data
 
 
-def main(unix_file, pickle_file):
+def main(push_info_file, sample_file):
 
-    past_unix_timestamp = ""
-    sleep_mode = False
-    past_sleep_mode = False
-    first_sleep_mode = ""
-    existing_txt_file = os.path.isfile("sampleSets/"+unix_file)
-    existing_pkl_file = os.path.isfile("sampleSets/"+pickle_file)
+    last_active_dump = 0
+    last_sleep = 0
+
+    existing_txt_file = os.path.isfile("sampleSets/"+push_info_file)
+    existing_pkl_file = os.path.isfile("sampleSets/"+sample_file)
+
     if (not existing_txt_file):
-        file = open("sampleSets/"+unix_file, "x")
+        file = open("sampleSets/"+push_info_file, "x")
         file.close()
+        with open("sampleSets/"+push_info_file, "wb") as file:
+            temp = {"last_active_dump":0.0, "last_sleep":0.0, "sleep_mode": False}
+            pickle.dump(temp, file)
+
     if (not existing_pkl_file):
-        file = open("sampleSets/"+pickle_file, "x")
+        file = open("sampleSets/"+sample_file, "x")
         file.close()
-        with open("sampleSets/"+pickle_file, "wb") as file:
+        with open("sampleSets/"+sample_file, "wb") as file:
             temp = [{}, {}, {}]
             pickle.dump(temp, file)
     
-    with open("sampleSets/"+unix_file, "r") as file:
-        lines = file.readlines()
-        if (len(lines) != 0):
-            past_unix_timestamp = lines[0]
-            sleep_mode_data = lines[1].split(",")
-            past_sleep_mode = bool(sleep_mode_data[0])
-            first_sleep_mode = sleep_mode_data[1]
+    push_info = {}
+    with open("sampleSets/"+push_info_file, "rb") as file:
+        push_info = pickle.load(file)
+
+    past_tag_values = {}
     
-    past_tag_values = []
-    
-    with open("sampleSets/"+pickle_file, "rb") as file:
+    with open("sampleSets/"+sample_file, "rb") as file:
         past_tag_values = pickle.load(file)
+    
+    print(past_tag_values)
 
-    if (past_unix_timestamp == ""):
-        with open("sampleSets/"+unix_file, "w+") as file:
-            file.writelines(str(curr_unix_timestamp))
-            past_unix_timestamp = curr_unix_timestamp - 5 * 60
-            file.writelines(str(sleep_mode) + "," + str(curr_unix_timestamp))
-        
-    else:
-        past_unix_timestamp = float(past_unix_timestamp)
-        first_sleep_mode = float(first_sleep_mode)
-
-    tag_values = ecall.getTags()
+    tag_values = pullData()
     new_push = compare_data_values(past_tag_values, tag_values)
     
     past_tag_values[2] = past_tag_values[1]
@@ -157,29 +147,32 @@ def main(unix_file, pickle_file):
     #         sleep_mode = True
 
     
-    if (new_push):
-        dbTagsPush(tag_values)
-    else:
-        # if past push was 30 mins ago and there was no active pushes within that time 
-        # enter sleep mode else push
-        sleep_mode = True
+    # if (new_push):
+    #     dbTagsPush(tag_values)
+    #     last_active_dump = curr_unix_timestamp
+    #     sleep_mode = False
+    # else:
+    #     # if past push was 30 mins ago and there was no active pushes within that time 
+    #     # enter sleep mode else push
+    #     if(last_active_dump + 30 > curr_unix_timestamp):
+    #         dbTagsPush(tag_values)
+    #     sleep_mode = True
 
-    with open("sampleSets/"+unix_file, "w+") as file:
-        file.writelines(str(curr_unix_timestamp)+"\n")
-        if (not sleep_mode):
-            file.writelines(str(sleep_mode)+","+str(curr_unix_timestamp))
-        else:
-            file.writelines(str(sleep_mode) + "," + str(first_sleep_mode))
+    # with open("sampleSets/"+push_info_file, "wb") as file:
+    #     file.writelines(str(curr_unix_timestamp)+"\n")
+    #     if (not sleep_mode):
+    #         file.writelines(str(sleep_mode)+","+str(curr_unix_timestamp))
+    #     else:
+    #         file.writelines(str(sleep_mode) + "," + str(first_sleep_mode))
         
     
-    with open("sampleSets/"+pickle_file, "wb") as file:
+    with open("sampleSets/"+sample_file, "wb") as file:
         pickle.dump(past_tag_values, file)
 
 if __name__ == "__main__":
     if (("-h" in sys.argv) or ("--help" in sys.argv)):
         print("Important: Please provide two files.")
-        print("One file must be a text file like \"batch.txt\"")
-        print("One must be a pickle file like \"pickle.pkl\"")
+        print("Add two pickle file arguments like: \"push_info.pkl\" and \"samples.pkl\"")
         print("Flags:")
         print("-d \ --debug\tUsed to enable debug mode (disabled by default)")
         exit()
@@ -191,5 +184,5 @@ if __name__ == "__main__":
         print("Debug mode: ON")
         DEBUG = True
 
-    dbcall.db_init()
+    # dbcall.db_init()
     main(sys.argv[1], sys.argv[2])
--- database/dbfuncs.py
+++ database/dbfuncs.py
@@ -12,6 +12,7 @@ def db_connection():
             password=db.PASSWORD,
             host=db.HOST,
             port=db.PORT,
+            connect_timeout=3,
         )
         cursor = conn.cursor()
         conn.autocommit = True
@@ -22,56 +23,202 @@ def db_connection():
 def db_init():
     conn, cursor = db_connection()
     
-    db_query = """CREATE TABLE IF NOT EXISTS position_table_prod (
-    tag_id VARCHAR(10),
-    WIPbase INT,
-    WIPvar INT,
-    x_coord FLOAT,
-    y_coord FLOAT,
-    z_coord FLOAT,
-    unixtime INT
+    db_query = """CREATE TABLE IF NOT EXISTS tblOrders (
+    WIP INT NOT NULL,
+    QTY INT NOT NULL,
+    TagID VARCHAR(6),
+    InProd BOOLEAN,
+    t_start FLOAT,
+    t_end FLOAT,
+    time_on_floor FLOAT,
+    build_time FLOAT,
+    LastZone INT,
+    ZoneName VARCHAR(50)
     )"""
 
     cursor.execute(db_query)
 
 
-    db_query = """CREATE TABLE IF NOT EXISTS position_table_arena (
-    tag_id VARCHAR(10),
-    x_coord FLOAT,
-    y_coord FLOAT,
-    z_coord FLOAT,
-    unixtime INT
+    db_query = """CREATE TABLE IF NOT EXISTS tblPaths (
+    WIP INT NOT NULL,
+    QTY INT NOT NULL,
+    TagID VARCHAR(6),
+    ZoneID INT,
+    ZoneName VARCHAR(50),
+    Time FLOAT
     )"""
 
     cursor.execute(db_query)
 
 
-    db_query = """CREATE TABLE IF NOT EXISTS user_roles (
-    role VARCHAR(10),
-    email VARCHAR(30),
-    first_name VARCHAR(15),
-    last_name VARCHAR(15)
+    db_query = """CREATE TABLE IF NOT EXISTS tblRawLocations (
+    WIP INT NOT NULL,
+    QTY INT NOT NULL,
+    TagID VARCHAR(6),
+    Timestamp FLOAT,
+    x FLOAT,
+    y FLOAT,
+    ZoneID INT
     )"""
+    
+    cursor.execute(db_query)
 
+    db_query = """CREATE TABLE IF NOT EXISTS tblZoneDef (
+    ZoneID SERIAL PRIMARY KEY NOT NULL,
+    ZoneName VARCHAR(50) NOT NULL,
+    x_lower FLOAT NOT NULL,
+    x_upper FLOAT NOT NULL,
+    y_lower FLOAT NOT NULL,
+    y_upper FLOAT NOT NULL
+    );"""
+
+    cursor.execute(db_query)
+    conn.close()
+
+def define_hard_zones():
+    conn, cursor = db_connection()
+    
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Repairs', 0, 24, 69, 100);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Missing Components', 1, 21, 34, 56);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Color Quarter Re-Order', 11, 32, 16, 26);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Special Assembly', 23, 44, 77, 100);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Special Assembly Stitching', 44.5, 59, 77, 100);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Assembly', 23, 44, 64, 77);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Final Assembly', 44, 59, 64, 77);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('On the Line 2', 25, 44, 26, 63);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Cleaning 2', 21, 34, 26, 37);"""
+    
+    cursor.execute(db_query)
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Runners', 31, 59, 0, 16);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Lasting', 60, 76, 70, 85);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Tacking', 60, 76, 85, 100);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Outsole', 77, 89, 63, 100);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Liner', 60, 77, 49, 70);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Blade', 76, 89, 51, 63);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Tongue Assembly', 59, 75, 28, 49);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Cleaning 1', 76, 89, 40, 50);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Inspection', 76, 89, 29, 40);"""
+    
+    cursor.execute(db_query)
+
+    db_query = """INSERT INTO tblZoneDef (ZoneName, x_lower, x_upper, y_lower, y_upper)
+    VALUES ('Pacing', 60, 89, 17, 28);"""
+    
     cursor.execute(db_query)
     conn.close()
 
 def delete_all():
    conn, cursor = db_connection()
 
-   db_query = "DROP TABLE position_table_prod"
+   db_query = "DROP TABLE IF EXISTS tblOrders"
 
    cursor.execute(db_query)
 
-   db_query = "DROP TABLE position_table_arena"
+   db_query = "DROP TABLE IF EXISTS tblPaths"
 
    cursor.execute(db_query)
    
-   db_query = "DROP TABLE user_roles"
+   db_query = "DROP TABLE IF EXISTS tblRawLocations"
+
+   cursor.execute(db_query)
+
+   db_query = "DROP TABLE IF EXISTS tblZoneDef"
 
    cursor.execute(db_query)
 
    conn.close()
+
+def dbPushTblOrders(WIP, QTY, tagID, inProd, t_start, t_end, time_on_floor, build_time, lastZone, zoneName):
+    conn, cursor = db_connection()
+    db_query = f"""INSERT INTO tblOrders (
+    {WIP}, {QTY}, '{tagID},' {inProd}, {t_start}, {t_end}, {time_on_floor}, {build_time}, {lastZone}, {zoneName} 
+    );"""
+    cursor.execute(db_query)
+    conn.close()
+
+def dbPushTblPaths(WIP, QTY, tagID, zoneID, zoneName, time):
+    conn, cursor = db_connection()
+    db_query = f"""INSERT INTO tblPaths (
+    {WIP}, {QTY}, '{tagID}', {zoneID}, '{zoneName}', {time}
+    );"""
+
+    cursor.execute(db_query)
+    conn.close()
+
+
+def dbPushTblRawLocations(WIP, QTY, tagID, timestamp, x, y, zoneID):
+    conn, cursor = db_connection()
+    db_query = f"""INSERT INTO tblRawLocations (
+    {WIP}, {QTY}, '{tagID}', {timestamp}, {x}, {y}, {zoneID}
+    );"""
+    cursor.execute()
+    conn.close()
 
 # def getActiveTimes():
 #    conn, cursor = db_connection()
@@ -91,10 +238,3 @@ def delete_all():
 #        cursor.execute(db_query)
 #    
 #    conn.close()
-
-def dbPushPositionTableProd(tag_id, WIPbase, WIPvar, x_coord, y_coord, z_coord, unixtime):
-    conn, cursor = db_connection()
-    db_query = f"INSERT INTO position_table_prod ('{tag_id}', {WIPbase}, {WIPvar}, {x_coord}, {y_coord}, {z_coord}, {unixtime});"
-    cursor.execute(db_query)
-    conn.close()
-
