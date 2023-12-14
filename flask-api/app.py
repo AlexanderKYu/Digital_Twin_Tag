0389d6df17e994f2e19d324c4b0e71bc378ff1cc
gabrielbeaupre <72994398+gabrielbeaupre@users.noreply.github.com>
2023-12-14 13:07:34 -0500

    Update app.py

--- flask-api/app.py
+++ flask-api/app.py
@@ -286,4 +286,4 @@ def disconnected():
 if __name__ == '__main__':
      # socketio.run(app, debug=True,port=5000)
     # socketio.run(app)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='192.168.0.10', port=5000, debug=True)
