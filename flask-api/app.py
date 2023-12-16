d8658568b036712864371849474a04e45ba73f2f
gabrielbeaupre <72994398+gabrielbeaupre@users.noreply.github.com>
2023-12-16 16:55:54 -0500

    Update app.py

--- flask-api/app.py
+++ flask-api/app.py
@@ -26,7 +26,7 @@ clients = 0
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-#CORS(app,resources={r"/*":{"origins":"*"}})
+CORS(app,resources={r"/*":{"origins":"*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on_error_default
