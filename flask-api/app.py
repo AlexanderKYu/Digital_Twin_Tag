d99f9d0c24d37ca235b4934254fbe606f22b2827
gabrielbeaupre <72994398+gabrielbeaupre@users.noreply.github.com>
2023-12-20 01:41:46 -0500

    Update app.py

--- flask-api/app.py
+++ flask-api/app.py
@@ -284,5 +284,5 @@ def disconnected():
 
 if __name__ == '__main__':
      # socketio.run(app, debug=True,port=5000)
-    socketio.run(app, async_mode='gevent_uwsgi')
+    socketio.run(app)
     # socketio.run(app, host='0.0.0.0', port=5000, debug=True, )
